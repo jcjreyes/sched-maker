d1fcc772a38a13eaf87bdb5da3eaf4ff16aee8b0
justin <justinreyes13003@gmail.com>
2024-01-12 22:11:30 +0800

    init init init

---
+++ src/Timetable.tsx
@@ -0,0 +1,13 @@
+import { format } from "date-fns";
+
+const WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Fri", "Sat"];
+
+const Timetable = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Timetable;
