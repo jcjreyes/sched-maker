7b4bc52ee023ed8698e0fdf7d98e4388d8d00108
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2024-01-13 16:43:51 +0800

    added lines in schedule

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import './App.css';
-import sampleData from './util/sampleData';
-import { parseStudentSchedule } from './util/parseStudentSched';
-import SetWithContentEquality, { Subject } from './types/SubjectSet';
-import sectionSchedules from './schedules/schedules';
-import { Section } from './types/enums';
-import FullCalendar from '@fullcalendar/react';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import moment from 'moment';
+import "./App.css";
+import sampleData from "./util/sampleData";
+import { parseStudentSchedule } from "./util/parseStudentSched";
+import SetWithContentEquality, { Subject } from "./types/SubjectSet";
+import sectionSchedules from "./schedules/schedules";
+import { Section } from "./types/enums";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import moment from "moment";
 
 function App() {
   const studentSched: string = sampleData;
   const subjectSet = new SetWithContentEquality<Subject>(
-    (subject) => subject.code,
+    (subject) => subject.code
   );
   const subjectArray = parseStudentSchedule(studentSched);
 
   subjectArray.forEach((subject) => {
-    const subjectDetails = subject.split('\n');
+    const subjectDetails = subject.split("\n");
     const subjectCode = subjectDetails[0];
-    const subjectSection = subjectDetails[1].split(' ')[0];
-    const subjectLoc = subjectDetails[1].split(' ')[1];
+    const subjectSection = subjectDetails[1].split(" ")[0];
+    const subjectLoc = subjectDetails[1].split(" ")[1];
 
     const final: Subject = {
       code: subjectCode,
@@ -33,7 +33,7 @@ function App() {
   const calendarItems = [];
   subjectSet.items.forEach((subject) => {
     const sectionKey = subject.section as Section;
-    const cleanedSectionKey = sectionKey.replace('-', '');
+    const cleanedSectionKey = sectionKey.replace("-", "");
     console.log(subject, sectionSchedules[cleanedSectionKey]);
 
     const combinedDetails = {
@@ -41,10 +41,10 @@ function App() {
       daysOfWeek: sectionSchedules[sectionKey].days,
       startTime: moment(
         sectionSchedules[sectionKey].startTime,
-        'hh:mm A',
-      ).format('HH:mm:ss'),
-      endTime: moment(sectionSchedules[sectionKey].endTime, 'hh:mm A').format(
-        'HH:mm:ss',
+        "hh:mm A"
+      ).format("HH:mm:ss"),
+      endTime: moment(sectionSchedules[sectionKey].endTime, "hh:mm A").format(
+        "HH:mm:ss"
       ),
     };
 
@@ -53,17 +53,29 @@ function App() {
 
   console.log(calendarItems);
 
+  // Find the minimum start time among all events
+  const minStartTime = moment.min(
+    calendarItems.map((item) => moment(item.startTime, "HH:mm:ss"))
+  );
+
   return (
     <>
-      <div className='actual-calendar'>
+      <div className="actual-calendar">
+        <div className="time-labels">
+          {Array.from({ length: 24 }).map((_, index) => (
+            <div key={index} className="time-label">
+              {minStartTime.clone().add(index, "hours").format("HH:mm")}
+            </div>
+          ))}
+        </div>
         <FullCalendar
           plugins={[timeGridPlugin]}
           headerToolbar={false}
           allDaySlot={false}
-          dayHeaderContent={(args) => moment(args.date).format('ddd')}
-          slotMinTime='07:30'
-          slotMaxTime='20:00'
-          slotDuration='00:20:00'
+          dayHeaderContent={(args) => moment(args.date).format("ddd")}
+          slotMinTime="07:30"
+          slotMaxTime="20:00"
+          slotDuration="00:20:00"
           events={calendarItems}
           hiddenDays={[0, 6]}
           // height={"auto"}
